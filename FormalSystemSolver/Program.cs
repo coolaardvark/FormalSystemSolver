ace3895b5e410cf25469f303267f99750fbda3cf
Mark Keightley <mark@keightley.net>
2020-07-26 15:43:16 +0100

    Initial code commit

---
+++ FormalSystemSolver/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+namespace FormalSystemSolver
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var startString = "MU";
+            var targetString = "MI";
+
+            // Build, or load, rules
+            var allRules = new List<Rule>();
+            // For the moment, I'll just hard code these for the MU system
+            // Dam I can't 'do' the I rule with just a regex, not sure how
+            // do this now
+            allRules.Add(new Rule(@"I\^", @"I\^"));
+            // I can't do this rule either, need a completely new approch
+            // here, I need a mini-language to descript the result string
+            // it will likely invovle regex meta characters, but needs more
+            // power than I can get from regex's alone
+            allRules.Add(new Rule(@"M[M,U,I]*", "M([M,U,I]*)"));
+            allRules.Add(new Rule(@"III", @"U"));
+            allRules.Add(new Rule(@"UU", @""));
+            // Maybe the second parameter in our rule is a reference
+            // to a little function (in line) that returns the result?
+            // Is this doable in CSharp and is it the right way to do it?
+
+            // Indentify cadidates for rule application
+            var applicableRules = findApplicableRules(allRules, startString);
+
+            bool targetFound = false;
+            while (!targetFound)
+            {
+                // Iterate through all candidates applying rules
+                var workingString = "";
+
+                // Store child strings
+
+                // Do we have any matches for our target string
+                // if so print results and exit
+                if (workingString == targetString)
+                {
+                    Console.WriteLine("Found solution!");
+                    targetFound = true;
+                }
+            }
+        }
+
+        static List<Rule> findApplicableRules(List<Rule> ruleSet, string source)
+        {
+            var applicableRules = new List<Rule>();
+
+
+            return applicableRules;
+        }
+
+        static string applyRule(Rule rule, string source)
+        {
+            string result = "";
+
+            return result;
+        }
+    }
+}
---
+++ FormalSystemSolver/Rule.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace FormalSystemSolver
+{
+    public class Rule
+    {
+        public Regex Target { get; private set; }
+        public Regex Result { get; private set; }
+
+        public Rule(string target, string result)
+        {
+            Target = new Regex(target);
+            Result = new Regex(result);
+        }
+    }
+}
